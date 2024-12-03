a196981afbd0f9ea604cda46ff654d4a47f5f440
Eiko <eikochanowo@outlook.com>
2024-12-03 23:08:11 +0000

    Async utils

---
+++ src/MeowBot/Async.hs
@@ -0,0 +1,20 @@
+-- | This module provides some simple async related wrappers for MeowBot.
+module MeowBot.Async where
+
+import MeowBot.BotStructure
+import Control.Monad.Trans
+
+asyncPureIOBotAction :: IO [BotAction] -> Meow [BotAction]
+asyncPureIOBotAction = fmap (pure . BAPureAsync) . lift . async
+
+asyncMeowBotAction :: IO (Meow [BotAction]) -> Meow [BotAction]
+asyncMeowBotAction = fmap (pure . BAAsync) . lift . async
+
+-- | Separate the action into async IO action and the function to convert the result of the IO action to a list of BotActions.
+-- the first part will be handled asynchronously, will not block the main thread.
+-- the second part will be executed in the main thread, not asynchronously.
+asyncMeowBot 
+  :: IO a -- ^ The IO action to be executed asynchronously
+  -> (a -> Meow [BotAction]) -- ^ The function to convert the result of the IO action to a list of BotActions
+  -> Meow [BotAction] -- ^ The Meow action that will be executed asynchronously
+asyncMeowBot ioa f = asyncMeowBotAction $ fmap f ioa
