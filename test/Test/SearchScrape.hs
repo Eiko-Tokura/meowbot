d6e93807a34c48b9a1c1866f8d7407a1a06eabf6
Eiko <eikochanowo@outlook.com>
2025-02-15 05:05:02 +0000

    adding test for search scrape

---
+++ test/Test/SearchScrape.hs
@@ -0,0 +1,17 @@
+module Test.SearchScrape where
+
+import Search
+import Scrape
+import Test.Tasty
+import Test.Tasty.HUnit
+import External.ChatAPI.Tool.Search
+
+testSearchScrape :: TestTree
+testSearchScrape = testGroup "Search"
+  [ testCase "search" $ do
+      Just api <- readGoogleApiKey
+      res <- search api 5 "haskell"
+      case res of
+        Left err -> assertFailure $ "search failed: " ++ show err
+        Right r  -> print r
+  ]
