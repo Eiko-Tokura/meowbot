55008595beafdfdf72c483f0dd8a294d667844a7
Eiko <eikochanowo@outlook.com>
2025-09-23 23:15:58 +0100

    Refactoring Connection Manager into monad-effect

--- src/Module/ConnectionManager.hs
+++ src/Module/ConnectionManager.hs
@@ -1,10 +1,14 @@
 {-# LANGUAGE TypeFamilies, OverloadedStrings, TemplateHaskell, UndecidableInstances #-}
 module Module.ConnectionManager where
 
+
 import Control.Monad.Trans.ReaderState
 import Control.Monad.Logger
 import Control.Applicative
-import Module
+import Control.Monad.Effect
+import Control.System
+import Module.RS.QQ
+import Module.Logging
 import MeowBot.BotStructure
 import Network.HTTP.Client (Manager, newManager, managerResponseTimeout, responseTimeoutMicro)
 import Network.HTTP.Client.TLS
@@ -12,35 +16,62 @@ import Network.Connection
 import Network.TLS
 import Data.Default
 
-data ConnectionManagerModule
+[makeRModule__|
+ConnectionManagerModule
+  manager       :: Manager
+  customTimeout :: Int
+|]
 
-instance MeowModule r AllData ConnectionManagerModule where
-  data ModuleLocalState ConnectionManagerModule  = ConnectionManagerModuleL
-  data ModuleEarlyLocalState ConnectionManagerModule = ConnectionManagerEarlyLocalState
-  data ModuleGlobalState ConnectionManagerModule = ConnectionManagerModuleG { manager :: Manager, customTimeout :: Int }
-  data ModuleEvent ConnectionManagerModule       = ConnectionManagerEvent
-  data ModuleInitDataG ConnectionManagerModule   = ConnectionManagerInitDataG
-  data ModuleInitDataL ConnectionManagerModule   = ConnectionManagerInitDataL
+instance SystemModule ConnectionManagerModule where
+  data ModuleInitData ConnectionManagerModule = ConnectionManagerModuleInitData
+  data ModuleEvent    ConnectionManagerModule = ConnectionManagerModuleEvent
 
-  getInitDataG _ = (Just ConnectionManagerInitDataG, empty)
-
-  getInitDataL _ = (Just ConnectionManagerInitDataL, empty)
-
-  initModule _ _ = do
+instance
+  Dependency' c ConnectionManagerModule '[LoggingModule] mods
+  => Loadable c ConnectionManagerModule mods where
+  initModule _ = do
     let customTimeout = 120 * 1000000 -- 120 seconds in microseconds
     let customManagerSettings =
           (mkManagerSettings
             ( case def of  -- | Turn off forcing EMS since bilibili doesn't support it, weird.
                            -- The library author says tls is insecure without EMS, but we don't have a choice.
-                t@TLSSettingsSimple{} -> t { settingClientSupported = def { supportedExtendedMainSecret = AllowEMS } }
-                t -> t
-            )
+               t@TLSSettingsSimple{} -> t { settingClientSupported = def { supportedExtendedMainSecret = AllowEMS } }
+               t -> t
+           )
             Nothing
           ){ managerResponseTimeout = responseTimeoutMicro customTimeout }
     manager <- liftIO $ newManager customManagerSettings
-    $(logInfo) "Connection Manager Initialized"
-    return $ ConnectionManagerModuleG manager customTimeout
+    $logInfo "Connection Manager Initialized"
+    return $ (ConnectionManagerModuleRead manager customTimeout, ConnectionManagerModuleState)
+  
 
-  initModuleLocal _ _ _ _ _ = return ConnectionManagerModuleL
-
-  initModuleEarlyLocal _ _ _ = return ConnectionManagerEarlyLocalState
+-- instance MeowModule r AllData ConnectionManagerModule where
+--   data ModuleLocalState ConnectionManagerModule  = ConnectionManagerModuleL
+--   data ModuleEarlyLocalState ConnectionManagerModule = ConnectionManagerEarlyLocalState
+--   data ModuleGlobalState ConnectionManagerModule = ConnectionManagerModuleG { manager :: Manager, customTimeout :: Int }
+--   data ModuleEvent ConnectionManagerModule       = ConnectionManagerEvent
+--   data ModuleInitDataG ConnectionManagerModule   = ConnectionManagerInitDataG
+--   data ModuleInitDataL ConnectionManagerModule   = ConnectionManagerInitDataL
+-- 
+--   getInitDataG _ = (Just ConnectionManagerInitDataG, empty)
+-- 
+--   getInitDataL _ = (Just ConnectionManagerInitDataL, empty)
+-- 
+--   initModule _ _ = do
+--     let customTimeout = 120 * 1000000 -- 120 seconds in microseconds
+--     let customManagerSettings =
+--           (mkManagerSettings
+--             ( case def of  -- | Turn off forcing EMS since bilibili doesn't support it, weird.
+--                            -- The library author says tls is insecure without EMS, but we don't have a choice.
+--                 t@TLSSettingsSimple{} -> t { settingClientSupported = def { supportedExtendedMainSecret = AllowEMS } }
+--                 t -> t
+--             )
+--             Nothing
+--           ){ managerResponseTimeout = responseTimeoutMicro customTimeout }
+--     manager <- liftIO $ newManager customManagerSettings
+--     $(logInfo) "Connection Manager Initialized"
+--     return $ ConnectionManagerModuleG manager customTimeout
+-- 
+--   initModuleLocal _ _ _ _ _ = return ConnectionManagerModuleL
+-- 
+--   initModuleEarlyLocal _ _ _ = return ConnectionManagerEarlyLocalState
